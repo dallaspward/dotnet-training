ab888f7864660c8d9ee7f91cb69f9a3fcc811a83
dallas ward <dallaspward@gmail.com>
2019-10-24 15:37:12 -0500

    add 00_csharp folder

---
+++ 00_csharp/HelloWorld/HelloWorld.Client/Program.cs
@@ -0,0 +1,19 @@
+﻿using sc = System.Console; //import with alias
+
+namespace HelloWorld.Client
+{
+  /// <summary>
+  /// Contains the start of the program
+  /// </summary>
+  internal class Program
+  {
+    /// <summary>
+    /// starts the program
+    /// </summary>
+
+     private static void Main(string[] args)
+    {
+        sc.WriteLine("Hello World!");
+    }
+  }
+}
